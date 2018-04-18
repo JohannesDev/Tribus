eefd892d7103026cbc416734dbb81617b7140a83
Johannes <33333384+JohannesDev@users.noreply.github.com>
2018-04-18 19:58:29 +0200

    created general python websocket

---
+++ sever.py
@@ -0,0 +1,18 @@
+import asyncio
+import random
+import websockets
+
+async def time(websocket, path):
+    while True:
+        r = lambda: random.randint(0,255)
+        color = '#%02X%02X%02X' % (r(),r(),r())
+
+        await websocket.send(color)
+        print("Color: ", color)
+        await asyncio.sleep(5)
+
+
+start_server = websockets.serve(time, '127.0.0.1', 5678)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
