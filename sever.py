3cc6e288558d9e3354c474c753953d1f22241430
Johannes <33333384+JohannesDev@users.noreply.github.com>
2018-04-19 08:53:56 +0200

    changed function name

--- sever.py
+++ sever.py
@@ -2,7 +2,7 @@ import asyncio
 import random
 import websockets
 
-async def time(websocket, path):
+async def sendMessage(websocket, path):
     while True:
         r = lambda: random.randint(0,255)
         color = '#%02X%02X%02X' % (r(),r(),r())
@@ -12,7 +12,7 @@ async def time(websocket, path):
         await asyncio.sleep(5)
 
 
-start_server = websockets.serve(time, '127.0.0.1', 5678)
+start_server = websockets.serve(sendMessage, '127.0.0.1', 5678)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
